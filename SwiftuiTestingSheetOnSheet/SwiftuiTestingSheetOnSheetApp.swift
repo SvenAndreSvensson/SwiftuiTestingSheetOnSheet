1294be7ea06e72ff8aeaf91ff1492c3714113d2c
Sven Andre Svensson <sven+git@bringsrud.net>
2021-06-28 09:55:53 +0200

    Initial Commit

---
+++ SwiftuiTestingSheetOnSheet/SwiftuiTestingSheetOnSheetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftuiTestingSheetOnSheetApp.swift
+//  SwiftuiTestingSheetOnSheet
+//
+//  Created by Sven Svensson on 28/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftuiTestingSheetOnSheetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
